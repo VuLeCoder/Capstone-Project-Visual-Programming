97040559820d7ccac10a725ffeded721e0d409a4
VuzLe <tle63917@gmail.com>
2025-10-27 20:05:48 +0700

    feat: thêm lịch cá nhân (giao diện)

--- BtlApp/FormMyTask.Designer.cs
+++ BtlApp/FormMyTask.Designer.cs
@@ -30,24 +30,26 @@
         {
             this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
             this.btn_createTask = new Sunny.UI.UIButton();
+            this.uiGroupBox1 = new Sunny.UI.UIGroupBox();
             this.tlp_mainCalendar = new System.Windows.Forms.TableLayoutPanel();
+            this.uiGroupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // monthCalendar1
             // 
             this.monthCalendar1.BackColor = System.Drawing.Color.Black;
             this.monthCalendar1.ForeColor = System.Drawing.Color.White;
-            this.monthCalendar1.Location = new System.Drawing.Point(18, 114);
+            this.monthCalendar1.Location = new System.Drawing.Point(0, 101);
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 0;
             // 
             // btn_createTask
             // 
-            this.btn_createTask.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_createTask.Cursor = System.Windows.Forms.Cursors.WaitCursor;
             this.btn_createTask.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(103)))));
             this.btn_createTask.FillColor2 = System.Drawing.Color.White;
             this.btn_createTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.btn_createTask.Location = new System.Drawing.Point(64, 43);
+            this.btn_createTask.Location = new System.Drawing.Point(67, 35);
             this.btn_createTask.MinimumSize = new System.Drawing.Size(1, 1);
             this.btn_createTask.Name = "btn_createTask";
             this.btn_createTask.Radius = 20;
@@ -57,66 +59,59 @@
             this.btn_createTask.Text = "Tạo";
             this.btn_createTask.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             // 
+            // uiGroupBox1
+            // 
+            this.uiGroupBox1.Controls.Add(this.monthCalendar1);
+            this.uiGroupBox1.Controls.Add(this.btn_createTask);
+            this.uiGroupBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.uiGroupBox1.FillColor = System.Drawing.Color.White;
+            this.uiGroupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.uiGroupBox1.Location = new System.Drawing.Point(10, 10);
+            this.uiGroupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiGroupBox1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiGroupBox1.Name = "uiGroupBox1";
+            this.uiGroupBox1.Padding = new System.Windows.Forms.Padding(0, 32, 0, 0);
+            this.uiGroupBox1.RectColor = System.Drawing.Color.White;
+            this.uiGroupBox1.Size = new System.Drawing.Size(262, 578);
+            this.uiGroupBox1.TabIndex = 2;
+            this.uiGroupBox1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
             // tlp_mainCalendar
             // 
             this.tlp_mainCalendar.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Single;
             this.tlp_mainCalendar.ColumnCount = 8;
-            this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 29.83333F));
-            this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70.16666F));
-            this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 367F));
+            this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.Dock = System.Windows.Forms.DockStyle.Right;
-            this.tlp_mainCalendar.Location = new System.Drawing.Point(259, 10);
+            this.tlp_mainCalendar.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tlp_mainCalendar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlp_mainCalendar.Location = new System.Drawing.Point(272, 10);
             this.tlp_mainCalendar.Name = "tlp_mainCalendar";
-            this.tlp_mainCalendar.RowCount = 25;
+            this.tlp_mainCalendar.RowCount = 2;
             this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tlp_mainCalendar.Size = new System.Drawing.Size(720, 578);
-            this.tlp_mainCalendar.TabIndex = 2;
+            this.tlp_mainCalendar.Size = new System.Drawing.Size(942, 578);
+            this.tlp_mainCalendar.TabIndex = 3;
             // 
             // FormMyTask
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(989, 598);
+            this.ClientSize = new System.Drawing.Size(1224, 598);
             this.Controls.Add(this.tlp_mainCalendar);
-            this.Controls.Add(this.btn_createTask);
-            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.uiGroupBox1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.ForeColor = System.Drawing.Color.White;
             this.Name = "FormMyTask";
             this.Padding = new System.Windows.Forms.Padding(10);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MyTask";
-            this.UseWaitCursor = true;
+            this.uiGroupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -125,6 +120,7 @@
 
         private System.Windows.Forms.MonthCalendar monthCalendar1;
         private Sunny.UI.UIButton btn_createTask;
+        private Sunny.UI.UIGroupBox uiGroupBox1;
         private System.Windows.Forms.TableLayoutPanel tlp_mainCalendar;
     }
 }
--- BtlApp/FormMyTask.cs
+++ BtlApp/FormMyTask.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Sunny.UI;
 
 namespace BtlApp
 {
@@ -15,6 +16,103 @@ namespace BtlApp
         public FormMyTask()
         {
             InitializeComponent();
+            createCalendar();
         }
+
+// ====================== ... ======================
+        private DateTime GetStartOfWeek(DateTime date)
+        {
+            int diff = date.DayOfWeek - DayOfWeek.Sunday;
+            if (diff < 0) diff += 7;
+            return date.AddDays(-diff).Date;
+        }
+// =====================================================================
+
+
+// ====================== ... ======================
+        private readonly int WEEK = 7, HOUR = 24;
+        private readonly string[] dayNames = { "CN", "TH 2", "TH 3", "TH 4", "TH 5", "TH 6", "TH 7" };
+        private void createHeaderCalendar()
+        {
+            DateTime currentWeekStart = GetStartOfWeek(DateTime.Now);
+            DateTime date;
+            
+            for (int i=0; i<WEEK; ++i)
+            {
+                date = currentWeekStart.AddDays(i);
+                Label dayLabel = new Label
+                {
+                    Text = $"{dayNames[(int)date.DayOfWeek]}\n{date:dd/MM/yyyy}",
+                    Margin = new Padding(0),
+                    Dock = DockStyle.Fill,
+                    Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold),
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    BackColor = date.Date == DateTime.Now.Date ?
+                    Color.FromArgb(144, 202, 249) : Color.FromArgb(240, 240, 240),
+                    ForeColor = date.Date == DateTime.Now.Date ? Color.White : Color.Black
+                };
+                tlp_mainCalendar.Controls.Add(dayLabel, i + 1, 0);
+            }
+        }
+
+        private void createTimeSlotCalendar()
+        {
+            int hour;
+            for(int h=0; h<HOUR; ++h)
+            {
+                hour = (h == 12) ? h : h % 12;
+                Label timeLabel = new Label
+                {
+                    Text = $"{hour} AM".Replace("AM", h >= 12 ? "PM" : "AM"),
+                    Dock = DockStyle.Fill,
+                    Font = new Font("Microsoft Sans Serif", 8F),
+                    TextAlign = ContentAlignment.TopRight,
+                    Padding = new Padding(5),
+                    BackColor = Color.FromArgb(250, 250, 250)
+                };
+                tlp_mainCalendar.Controls.Add(timeLabel, 0, h + 1);
+
+                for(int d=0; d<WEEK; ++d)
+                {
+                    UIPanel panel = new UIPanel
+                    {
+                        Dock = DockStyle.Fill,
+                        FillColor = Color.White,
+                        RectColor = Color.FromArgb(230, 230, 230),
+                        Radius = 10,
+                        Margin = new Padding(0),
+                        Tag = new { Day = d, Hour = hour }
+                    };
+                    //panel.MouseClick += Panel_MouseClick;
+                    tlp_mainCalendar.Controls.Add(panel, d + 1, h + 1);
+                }
+            }
+        }
+
+        private void createCalendar()
+        {
+            tlp_mainCalendar.ColumnCount = 8;
+            tlp_mainCalendar.RowCount = 25;
+
+            tlp_mainCalendar.Controls.Clear();
+            tlp_mainCalendar.ColumnStyles.Clear();
+            tlp_mainCalendar.RowStyles.Clear();
+
+            tlp_mainCalendar.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 80));
+            for(int i=0; i<WEEK; ++i)
+            {
+                tlp_mainCalendar.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 14.28F));
+            }
+
+            tlp_mainCalendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 50));
+            for(int i=0; i<HOUR; ++i)
+            {
+                tlp_mainCalendar.RowStyles.Add(new RowStyle(SizeType.Absolute, 60));
+            }
+
+            createHeaderCalendar();
+            createTimeSlotCalendar();
+        }
+// =====================================================================
     }
 }
