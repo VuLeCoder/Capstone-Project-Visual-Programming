3e833d8467887b9056b27d6748f4006fdca03afb
VuzLe <tle63917@gmail.com>
2025-11-01 22:39:49 +0700

    feat: Quản lý tên, thuộc tính bảng trong database

---
+++ BtlApp/Database/DbTables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtlApp.Database
+{
+    internal class DbTables
+    {
+        public static class tbl_TimeSLot
+        {
+            public const string Table = "tbl_TimeSlot";
+            public const string TimeStr = "TimeStr";
+            public const string TimeVal = "TimeVal";
+        }
+
+        public static class tbl_User 
+        {
+            public const string Table = "tbl_User"; 
+            public const string Id = "UserId"; 
+            public const string Name = "UserName"; 
+            public const string Email = "Email"; 
+            public const string Password = "PasswordHash"; 
+        }
+
+    }
+}
