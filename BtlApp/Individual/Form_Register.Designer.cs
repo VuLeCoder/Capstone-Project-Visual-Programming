8d4176bfb360c3f641a2e57a81c6202e864e4679
VuzLe <tle63917@gmail.com>
2025-11-01 23:35:36 +0700

    feat: kiểm tra valid và đăng ký người dùng

--- BtlApp/Individual/Form_Register.Designer.cs
+++ BtlApp/Individual/Form_Register.Designer.cs
@@ -36,8 +36,8 @@
             this.txtEmail = new System.Windows.Forms.TextBox();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.txtRePassword = new System.Windows.Forms.TextBox();
-            this.createBtn = new Sunny.UI.UIButton();
-            this.linkAcc = new System.Windows.Forms.LinkLabel();
+            this.btn_Register = new Sunny.UI.UIButton();
+            this.link_BackLogin = new System.Windows.Forms.LinkLabel();
             this.lblPasswordError = new System.Windows.Forms.Label();
             this.lblConfirmError = new System.Windows.Forms.Label();
             this.SuspendLayout();
@@ -46,21 +46,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(104, 57);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(78, 46);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 25);
+            this.label1.Size = new System.Drawing.Size(83, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Username";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label2.Location = new System.Drawing.Point(104, 111);
+            this.label2.Location = new System.Drawing.Point(78, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(60, 25);
+            this.label2.Size = new System.Drawing.Size(48, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Email";
             // 
@@ -68,9 +67,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label3.Location = new System.Drawing.Point(104, 165);
+            this.label3.Location = new System.Drawing.Point(78, 134);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(98, 25);
+            this.label3.Size = new System.Drawing.Size(78, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "Password";
             // 
@@ -78,35 +78,39 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.label4.Location = new System.Drawing.Point(104, 225);
+            this.label4.Location = new System.Drawing.Point(78, 183);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(129, 25);
+            this.label4.Size = new System.Drawing.Size(104, 20);
             this.label4.TabIndex = 3;
             this.label4.Text = "Re-Password";
             // 
             // txtUser
             // 
             this.txtUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.txtUser.Location = new System.Drawing.Point(312, 54);
+            this.txtUser.Location = new System.Drawing.Point(234, 44);
+            this.txtUser.Margin = new System.Windows.Forms.Padding(2);
             this.txtUser.Name = "txtUser";
-            this.txtUser.Size = new System.Drawing.Size(277, 30);
+            this.txtUser.Size = new System.Drawing.Size(209, 26);
             this.txtUser.TabIndex = 4;
             // 
             // txtEmail
             // 
             this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.txtEmail.Location = new System.Drawing.Point(312, 111);
+            this.txtEmail.Location = new System.Drawing.Point(234, 90);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(277, 30);
+            this.txtEmail.Size = new System.Drawing.Size(209, 26);
             this.txtEmail.TabIndex = 5;
             this.txtEmail.TextChanged += new System.EventHandler(this.txtEmail_TextChanged);
             // 
             // txtPassword
             // 
             this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.txtPassword.Location = new System.Drawing.Point(312, 165);
+            this.txtPassword.Location = new System.Drawing.Point(234, 134);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(277, 30);
+            this.txtPassword.Size = new System.Drawing.Size(209, 26);
             this.txtPassword.TabIndex = 6;
             this.txtPassword.UseSystemPasswordChar = true;
             this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
@@ -114,63 +118,69 @@
             // txtRePassword
             // 
             this.txtRePassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.txtRePassword.Location = new System.Drawing.Point(312, 222);
+            this.txtRePassword.Location = new System.Drawing.Point(234, 180);
+            this.txtRePassword.Margin = new System.Windows.Forms.Padding(2);
             this.txtRePassword.Name = "txtRePassword";
-            this.txtRePassword.Size = new System.Drawing.Size(277, 30);
+            this.txtRePassword.Size = new System.Drawing.Size(209, 26);
             this.txtRePassword.TabIndex = 7;
             this.txtRePassword.UseSystemPasswordChar = true;
             this.txtRePassword.TextChanged += new System.EventHandler(this.txtRePassword_TextChanged);
             // 
-            // createBtn
+            // btn_Register
             // 
-            this.createBtn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.createBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
-            this.createBtn.Location = new System.Drawing.Point(269, 304);
-            this.createBtn.MinimumSize = new System.Drawing.Size(1, 1);
-            this.createBtn.Name = "createBtn";
-            this.createBtn.Radius = 20;
-            this.createBtn.Size = new System.Drawing.Size(178, 35);
-            this.createBtn.TabIndex = 8;
-            this.createBtn.Text = "Create Account";
-            this.createBtn.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btn_Register.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Register.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.btn_Register.Location = new System.Drawing.Point(179, 237);
+            this.btn_Register.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Register.MinimumSize = new System.Drawing.Size(1, 1);
+            this.btn_Register.Name = "btn_Register";
+            this.btn_Register.Radius = 20;
+            this.btn_Register.Size = new System.Drawing.Size(157, 39);
+            this.btn_Register.TabIndex = 8;
+            this.btn_Register.Text = "Create Account";
+            this.btn_Register.TipsFont = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.btn_Register.Click += new System.EventHandler(this.btn_Register_Click);
             // 
-            // linkAcc
+            // link_BackLogin
             // 
-            this.linkAcc.AutoSize = true;
-            this.linkAcc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.linkAcc.Location = new System.Drawing.Point(284, 355);
-            this.linkAcc.Name = "linkAcc";
-            this.linkAcc.Size = new System.Drawing.Size(146, 20);
-            this.linkAcc.TabIndex = 9;
-            this.linkAcc.TabStop = true;
-            this.linkAcc.Text = "Have an Account?";
-            this.linkAcc.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkAcc_LinkClicked);
+            this.link_BackLogin.AutoSize = true;
+            this.link_BackLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.link_BackLogin.Location = new System.Drawing.Point(195, 278);
+            this.link_BackLogin.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.link_BackLogin.Name = "link_BackLogin";
+            this.link_BackLogin.Size = new System.Drawing.Size(124, 17);
+            this.link_BackLogin.TabIndex = 9;
+            this.link_BackLogin.TabStop = true;
+            this.link_BackLogin.Text = "Have an Account?";
+            this.link_BackLogin.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.link_BackLogin_LinkClicked);
             // 
             // lblPasswordError
             // 
             this.lblPasswordError.AutoSize = true;
-            this.lblPasswordError.Location = new System.Drawing.Point(312, 200);
+            this.lblPasswordError.Location = new System.Drawing.Point(234, 162);
+            this.lblPasswordError.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPasswordError.Name = "lblPasswordError";
-            this.lblPasswordError.Size = new System.Drawing.Size(0, 16);
+            this.lblPasswordError.Size = new System.Drawing.Size(0, 13);
             this.lblPasswordError.TabIndex = 10;
             // 
             // lblConfirmError
             // 
             this.lblConfirmError.AutoSize = true;
-            this.lblConfirmError.Location = new System.Drawing.Point(312, 259);
+            this.lblConfirmError.Location = new System.Drawing.Point(234, 210);
+            this.lblConfirmError.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblConfirmError.Name = "lblConfirmError";
-            this.lblConfirmError.Size = new System.Drawing.Size(0, 16);
+            this.lblConfirmError.Size = new System.Drawing.Size(0, 13);
             this.lblConfirmError.TabIndex = 11;
             // 
             // Form_Register
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(715, 411);
+            this.ClientSize = new System.Drawing.Size(556, 334);
             this.Controls.Add(this.lblConfirmError);
             this.Controls.Add(this.lblPasswordError);
-            this.Controls.Add(this.linkAcc);
-            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.link_BackLogin);
+            this.Controls.Add(this.btn_Register);
             this.Controls.Add(this.txtRePassword);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.txtEmail);
@@ -179,10 +189,9 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form_Register";
             this.Text = "Register";
-            this.Load += new System.EventHandler(this.Form_Register_Load);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form_Register_FormClosed);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,8 +207,8 @@
         private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.TextBox txtRePassword;
-        private Sunny.UI.UIButton createBtn;
-        private System.Windows.Forms.LinkLabel linkAcc;
+        private Sunny.UI.UIButton btn_Register;
+        private System.Windows.Forms.LinkLabel link_BackLogin;
         private System.Windows.Forms.Label lblPasswordError;
         private System.Windows.Forms.Label lblConfirmError;
     }
--- BtlApp/Individual/Form_Register.cs
+++ BtlApp/Individual/Form_Register.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using BtlApp.Database;
+using BtlApp.Database.Models;
+using FormProduct.Classes;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,7 +18,15 @@ namespace BtlApp.Individual
     public partial class Form_Register : Form
     {
         private readonly Form_Login formLogin;
+        private readonly DataProcesser Db = new DataProcesser();
 
+        public Form_Register(Form_Login formLogin)
+        {
+            InitializeComponent();
+            this.formLogin = formLogin;
+        }
+
+        // ================= Hàm =====================
         private bool IsValidEmail(string email)
         {
             string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
@@ -27,29 +39,27 @@ namespace BtlApp.Individual
             return Regex.IsMatch(password, pattern);
         }
 
-        public Form_Register(Form_Login formLogin)
+        private bool IsFullFill()
         {
-            InitializeComponent();
-            this.formLogin = formLogin;
+            if (txtEmail.BackColor != Color.White)
+            {
+                txtEmail.Focus();
+                return false;
+            }
+
+            if (lblPasswordError.Text.Trim().Length != 0)
+            {
+                txtPassword.Focus();
+                return false;
+            }
+            if (lblConfirmError.Text.Trim().Length != 0)
+            {
+                txtRePassword.Focus();
+                return false;
+            }
+            return true;
         }
-
-        private void Form_Register_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-
-        }
-
-        private void linkAcc_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
-        { 
-            formLogin.Show();
-            this.Close();
-        }
-
+        // ================ Sự kiện =====================
         private void txtEmail_TextChanged(object sender, EventArgs e)
         {
             if (IsValidEmail(txtEmail.Text))
@@ -83,6 +93,81 @@ namespace BtlApp.Individual
                 lblConfirmError.Text = "Mật khẩu nhập lại không khớp!";
                 lblConfirmError.ForeColor = Color.Red;
             }
+        }
+
+        private void btn_Register_Click(object sender, EventArgs e)
+        {
+            if (!IsFullFill()) return;
+
+            string query =
+                $@"select {DbTables.tbl_User.Id}
+                    from {DbTables.tbl_User.Table}
+                    where {DbTables.tbl_User.Email} = @Email";
+
+            SqlParameter[] parameters = {
+                new SqlParameter("@Email", txtEmail.Text),
+            };
+
+            DataTable dt = Db.ReadTable(query, parameters);
+            if(dt.Rows.Count > 0 )
+            {
+                MessageBox.Show("Email này đã được đăng ký tài khoản", "Thông báo",
+                    MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtEmail.Focus();
+                return;
+            }
+
+            string username = txtUser.Text;
+            if(username.Trim().Length == 0)
+            {
+                username = txtEmail.Text;
+            }
+
+            try
+            {
+                query =
+                    $@"insert into {DbTables.tbl_User.Table} 
+                        ({DbTables.tbl_User.Name}, {DbTables.tbl_User.Email}, {DbTables.tbl_User.Password})
+                        values (@Username, @Email, @Password)";
+
+                SqlParameter[] InsertParameters = {
+                        new SqlParameter("@Username", username),
+                        new SqlParameter("@Email", txtEmail.Text),
+                        new SqlParameter("@Password", txtPassword.Text)
+                    };
+
+                int rowsAffected = Db.ExecuteNonQuery(query, InsertParameters);
+                if (rowsAffected > 0)
+                {
+                    // cout << :)))
+                    MessageBox.Show("Đăng ký thành công", "Information",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    this.Close();
+                }
+                else
+                {
+                    // cout << :)))
+                    MessageBox.Show("⚠️ Lỗi khi đăng ký", "Warning",
+                        MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }    
+            }
+            catch (Exception ex)
+            {
+                // cout << :)))
+                MessageBox.Show("❌ Lỗi khi đăng ký:\n" + ex.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void link_BackLogin_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form_Register_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            formLogin.Show();
         }
     }
 }
