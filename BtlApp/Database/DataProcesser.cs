35448c75765e50950b9fd6af9def7dab13483423
crysdrag <tungphamj@gmail.com>
2025-11-02 12:59:04 +0700

    !!!!!!!!!!@@@@@ Chỉnh lại db cho hợp máy tôi !!!!!!! Đọc kĩ trước khi merge

--- BtlApp/Database/DataProcesser.cs
+++ BtlApp/Database/DataProcesser.cs
@@ -8,7 +8,7 @@ namespace FormProduct.Classes
     internal class DataProcesser
     {
         private readonly string dbName = "Schedule";
-        private readonly string serverName = @"VUZ\SQLEXPRESS";
+        private readonly string serverName = @"(local)\SQLEXPRESS";
         private string connectionString =>
             $@"Server={serverName};Database={dbName};Trusted_Connection=True;";
 
